cc869c43baef037204992f871d7ac2c4837ebf2b
floydw1234 <floydw1234@gmail.com>
2025-04-09 22:08:22 -0600

    added utils

---
+++ src/packages/utils/print.c
@@ -0,0 +1,10 @@
+#include "print.h"
+#include <stdio.h>
+
+void print_hex(const char* label, const unsigned char* data, size_t len) {
+    printf("%s: ", label);
+    for (size_t i = 0; i < len; i++) {
+        printf("%02x", data[i]);
+    }
+    printf("\n");
+}
---
+++ src/packages/utils/print.h
@@ -0,0 +1,8 @@
+#ifndef PRINT_H
+#define PRINT_H
+
+#include <stddef.h>
+
+void print_hex(const char* label, const unsigned char* data, size_t len);
+
+#endif /* PRINT_H */
