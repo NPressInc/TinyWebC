6a36813a6e839d763e1ff8c1a6e9084b457e8bf4
root <root@williamsPc.>
2025-05-05 08:59:45 -0600

    buildablein wsl

--- cmake_install.cmake
+++ cmake_install.cmake
@@ -1,4 +1,4 @@
-# Install script for directory: /home/william/Documents/tinyweb/CTinyWeb
+# Install script for directory: /home/dolla/TinyWebC
 
 # Set the install prefix
 if(NOT DEFINED CMAKE_INSTALL_PREFIX)
@@ -50,5 +50,5 @@ endif()
 
 string(REPLACE ";" "\n" CMAKE_INSTALL_MANIFEST_CONTENT
        "${CMAKE_INSTALL_MANIFEST_FILES}")
-file(WRITE "/home/william/Documents/tinyweb/CTinyWeb/${CMAKE_INSTALL_MANIFEST}"
+file(WRITE "/home/dolla/TinyWebC/${CMAKE_INSTALL_MANIFEST}"
      "${CMAKE_INSTALL_MANIFEST_CONTENT}")
